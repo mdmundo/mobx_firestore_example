b8519a01cb79074d5a6a09bc882ad5997469eec3
Edmundo Paulino <manyymoore@gmail.com>
2021-10-26 10:06:53 -0300

    style: 🔥 Remove comments

--- lib/store.dart
+++ lib/store.dart
@@ -2,15 +2,12 @@ import 'dart:async';
 import 'package:mobx/mobx.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-// Include generated file
 part 'store.g.dart';
 
-// This is the class used by rest of your codebase
 class Token = _Token with _$Token;
 
 FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-// The store-class
 abstract class _Token with Store {
   _Token() {
     _streamController = StreamController<QuerySnapshot>();
